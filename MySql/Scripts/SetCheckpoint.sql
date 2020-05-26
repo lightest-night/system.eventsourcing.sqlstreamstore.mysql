de606c67994366010497469d624b4c12f43f34ca
James Woodley <james@lightestnight.com>
2020-05-26 16:37:22 +0100

    Use the MySql ON DUPLICATE KEY UPDATE syntax

--- MySql/Scripts/SetCheckpoint.sql
+++ MySql/Scripts/SetCheckpoint.sql
@@ -1,7 +1,3 @@
-﻿IF EXISTS ( SELECT checkpoint_id FROM checkpoints WHERE checkpoint_id = @CheckpointId )
-    UPDATE checkpoints
-    SET checkpoint = @Checkpoint
-    WHERE checkpoint_id = @CheckpointId;
-ELSE
-    INSERT INTO checkpoints (checkpoint_id, checkpoint)
-    VALUES (@CheckpointId, @Checkpoint);
+﻿INSERT INTO checkpoints (checkpoint_id, checkpoint)
+VALUES (@CheckpointId, @Checkpoint)
+ON DUPLICATE KEY UPDATE checkpoint = @Checkpoint;
