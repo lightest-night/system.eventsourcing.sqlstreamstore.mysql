01c5ec3f3f4903178e35de53175322e24ceda6ed
James Woodley <james@lightestnight.com>
2020-10-25 22:56:13 +0000

    Make sure schemas are created early

--- MySql/ExtendsServiceCollection.cs
+++ MySql/ExtendsServiceCollection.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Threading.Tasks;
 using LightestNight.System.Data.MySql;
 using LightestNight.System.EventSourcing.Checkpoints;
 using LightestNight.System.EventSourcing.SqlStreamStore.MySql.Checkpoints;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
+using SqlStreamStore;
+
 // ReSharper disable RedundantAssignment
 
 namespace LightestNight.System.EventSourcing.SqlStreamStore.MySql
@@ -23,10 +26,25 @@ namespace LightestNight.System.EventSourcing.SqlStreamStore.MySql
                 .AddTransient<IStreamStoreFactory>(sp =>
                 {
                     var connection = sp.GetRequiredService<IMySqlConnection>();
-                    var checkpointManager = sp.GetRequiredService<MySqlCheckpointManager>();
                     var logger = sp.GetRequiredService<ILoggerFactory>().CreateLogger<MySqlStreamStoreFactory>();
 
-                    return new MySqlStreamStoreFactory(connection, options, checkpointManager, logger);
+                    var factory = new MySqlStreamStoreFactory(connection, logger);
+
+                    if (!options.CreateSchemaIfNotExists)
+                        return factory;
+
+                    Task.Run(async () =>
+                    {
+                        var checkpointManager = sp.GetRequiredService<MySqlCheckpointManager>();
+                        if (!(await factory.GetStreamStore() is MySqlStreamStore streamStore))
+                            throw new InvalidOperationException(
+                                "Cannot create schema when Stream Store has not been defined.");
+
+                        await Task.WhenAll(streamStore.CreateSchemaIfNotExists(),
+                            checkpointManager.CreateSchemaIfNotExists()).ConfigureAwait(false);
+                    });
+
+                    return factory;
                 });
 
             services.TryAddSingleton<GetGlobalCheckpoint>(sp =>
--- MySql/MySqlStreamStoreFactory.cs
+++ MySql/MySqlStreamStoreFactory.cs
@@ -2,7 +2,6 @@
 using System.Threading;
 using System.Threading.Tasks;
 using LightestNight.System.Data.MySql;
-using LightestNight.System.EventSourcing.SqlStreamStore.MySql.Checkpoints;
 using Microsoft.Extensions.Logging;
 using SqlStreamStore;
 using MySqlConnection = MySqlConnector.MySqlConnection;
@@ -15,39 +14,28 @@ namespace LightestNight.System.EventSourcing.SqlStreamStore.MySql
         private static MySqlConnection? _mySqlConnection;
         
         private readonly IMySqlConnection _connection;
-        private readonly MySqlEventSourcingOptions _options;
-        private readonly MySqlCheckpointManager _checkpointManager;
         private readonly ILogger _logger;
-        
-        public MySqlStreamStoreFactory(IMySqlConnection connection, MySqlEventSourcingOptions options, MySqlCheckpointManager checkpointManager,
-            ILogger<MySqlStreamStoreFactory> logger)
+
+        public MySqlStreamStoreFactory(IMySqlConnection connection, ILogger<MySqlStreamStoreFactory> logger)
         {
             _connection = connection ?? throw new ArgumentNullException(nameof(connection));
-            _options = options ?? throw new ArgumentNullException(nameof(options));
-            _checkpointManager = checkpointManager ?? throw new ArgumentNullException(nameof(checkpointManager));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
-        public async Task<IStreamStore> GetStreamStore(int retries = 3, CancellationToken cancellationToken = default)
+        public Task<IStreamStore> GetStreamStore(int retries = 3, CancellationToken cancellationToken = default)
         {
             if (_streamStore != null && _mySqlConnection != null &&
                 _connection.ValidateConnection(_mySqlConnection, out _))
             {
                 _logger.LogInformation("Using existing StreamStore & Connection...");
-                return _streamStore;
+                return Task.FromResult(_streamStore);
             }
 
             _logger.LogInformation("Building new StreamStore & Connection");
             _mySqlConnection = _connection.GetConnection(retries);
             _streamStore = new MySqlStreamStore(new MySqlStreamStoreSettings(_mySqlConnection.ConnectionString));
 
-            if (!_options.CreateSchemaIfNotExists)
-                return _streamStore;
-
-            await Task.WhenAll(((MySqlStreamStore) _streamStore).CreateSchemaIfNotExists(cancellationToken),
-                _checkpointManager.CreateSchemaIfNotExists(cancellationToken)).ConfigureAwait(false);
-
-            return _streamStore;
+            return Task.FromResult(_streamStore);
         }
     }
 }
